f80406f5b01ddf57ec93a2fb65259c2d255eccf0
maqamylee0 <maqamylee0@gmail.com>
2023-11-24 13:56:54 +0300

    add tailwind css to layout

--- app/dashboard/nav-links.tsx
+++ app/dashboard/nav-links.tsx
@@ -1,35 +1,43 @@
-
+import clsx from "clsx";
 import { AiFillHome } from "react-icons/ai";
 
+const links = [
+  { name: "Home", href: "/dashboard", icon: AiFillHome },
+  {
+    name: "In Progress",
+    href: "/dashboard/inProgress",
+    icon: AiFillHome,
+  },
+  { name: "Completed", href: "/dashboard/completed", icon: AiFillHome },
+];
 
-  const links = [
-    { name: 'Home', href: '/dashboard', icon: AiFillHome },
-    {
-      name: 'InProgress',
-      href: '/dashboard/inProgress',
-    //   icon: DocumentDuplicateIcon,
-    },
-    { name: 'Completed', href: '/dashboard/completed',
-    //  icon: UserGroupIcon
-     },
-  ];
-  
-  export default function NavLinks() {
-    return (
-      <>
-        {links.map((link) => {
-          const LinkIcon = link.icon;
-          return (
-            <a
-              key={link.name}
-              href={link.href}
-              className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
-            >
-              {/* <LinkIcon className="w-6" /> */}
-              <p className="hidden md:block">{link.name}</p>
-            </a>
-          );
-        })}
-      </>
-    );
-  }
+export default function NavLinks() {
+  return (
+    <>
+      {links.map((link) => {
+        const LinkIcon = link.icon;
+        return (
+          <a
+            key={link.name}
+            href={link.href}
+            className={clsx(
+              "flex h-[48px] grow items-end p-3 text-sm font-medium bg-black hover:bg-sky-100 hover:text-blue-600 md:flex-none md:p-2 md:px-3",
+              {
+                "ml-2": link.name === "Home",
+                "ml-20": link.name !== "Home",
+              }
+            )}
+          >
+            <LinkIcon
+              className={clsx("w-10 h-7", {
+                "visible mr-2": link.name === "Home",
+                hidden: link.name !== "Home",
+              })}
+            />
+            <p className="md:block">{link.name}</p>
+          </a>
+        );
+      })}
+    </>
+  );
+}
--- app/dashboard/sidenav.tsx
+++ app/dashboard/sidenav.tsx
@@ -1,29 +1,34 @@
-import Link from 'next/link';
-import NavLinks from '@/app/dashboard/nav-links';
-// import AcmeLogo from '@/app/ui/acme-logo';
-// import { PowerIcon } from '@heroicons/react/24/outline';
+import Link from "next/link";
+import NavLinks from "@/app/dashboard/nav-links";
+import SkillerLogo from "@/components/ui/logo";
+// import { TiThMenuOutline } from "react-icons/ti";
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+    <div className="flex h-full flex-col text-white">
       <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
+        className="flex h-20 items-end justify-start space-x-40 bg-black p-4 md:h-40 md:justify-center"
         href="/"
       >
         <div className="w-32 text-white md:w-40">
-          {/* <AcmeLogo /> */}
+          <SkillerLogo />
         </div>
+        {/* <div className="w-32 h-10 text-white md:w-40 md:hidden">
+        <TiThMenuOutline/>
+        </div> */}
       </Link>
-      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+      {/* <div className="flex grow flex-row justify-center md:flex-col "> */}
+
+      <div className="h-auto w-full grow md:block bg-black">
         <NavLinks />
-        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <form>
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            {/* <PowerIcon className="w-6" /> */}
-            <div className="hidden md:block">Sign Out</div>
-          </button>
-        </form>
       </div>
+
+      <form>
+        <button className="flex h-[48px] w-full grow items-center justify-center bg-black p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+          <div className="md:block p-4">Sign Out</div>
+        </button>
+      </form>
+      {/* </div> */}
     </div>
   );
 }
