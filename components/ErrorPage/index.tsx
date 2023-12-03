ec9caded29580f432eb6bdabb10880572f3be253
maqamylee0 <maqamylee0@gmail.com>
2023-12-03 13:02:59 +0300

    add css and responsiveness

--- components/ErrorPage/index.tsx
+++ components/ErrorPage/index.tsx
@@ -14,20 +14,33 @@ export default function ErrorPage() {
   // }
 
   return (
-    <div className="flex w-full flex-col self-end md:flex w-full flex-col gap-[3rem] bg-grey h-screen justify-center relative items-center text-white">
-      <div className="relative w-[16rem] h-[3rem]">
+    <div className="flex flex-col justify-center md:grid md:grid-cols-3 md:grid-rows-3 gap-4  bg-grey h-screen p-10 relative text-white overflow-y-auto">
+      {/* <div className="grid grid-cols-3" > */}
+      <div className="md:w-20 "></div>
+      <div className="relative w-[16rem] h-[4rem] flex justify-end self-center md:hidden">
         <Image src={data.whitelogo} alt={"logo"} priority={true} fill />
       </div>
-
-      <div className="text-[40px] mt-[2rem] text-black">{data.tagline}</div>
-
-      <div className="relative w-[22rem] h-[12rem]">
-        <Image src={data.errorImage} alt={"logo"} priority={true} fill />
+      <div className="text-[40px] text-black flex justify-center self-center">
+        {data.tagline}
       </div>
-      <div className="text-[40px] mt-[2rem] text-black">
-        {data.errorMessage}
+
+      <div className="hidden md:relative  md:h-[4rem] md:flex md:self-start md:justify-end">
+        <Image src={data.whitelogo} alt={"logo"} priority={true} fill />
       </div>
-      <Button>{data.buttonText}</Button>
+      {/* </div> */}
+      <div className="md:w-20 h-300 row-span-2"></div>
+
+      <div className="flex w-full flex-col gap-[4rem] bg-grey items-center row-span-2">
+        <div className="relative w-[18rem] h-[12rem] flex self-center justify-center">
+          <Image src={data.errorImage} alt={"logo"} priority={true} fill />
+        </div>
+        <div className="text-[40px] mt-[2rem] text-black">
+          {data.errorMessage}
+        </div>
+        <Button size="lg">{data.buttonText}</Button>
+      </div>
+
+      <div className="md:w-20"></div>
     </div>
   );
 }
