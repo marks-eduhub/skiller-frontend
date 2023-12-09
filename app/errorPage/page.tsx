5eafd9297c666d414eda132fe91312d795f90840
maqamylee0 <maqamylee0@gmail.com>
2023-12-09 19:07:33 +0300

    add not found functionality for unfound routes

--- app/errorPage/page.tsx
+++
@@ -1,5 +0,0 @@
-import Error from "@/components/ErrorPage";
-
-export default function ErrorPage() {
-    return <Error/>;
-}
--- components/NotFoundPage/index.tsx
+++ components/NotFoundPage/index.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import { Button } from "../ui/button";
 // import { useRouter } from 'next/router';
 
-export default function ErrorPage() {
+export default function NotFoundPage() {
   // const router = useRouter(); // Access the router object
 
   // function handleOnClick(): void {
@@ -18,21 +18,21 @@ export default function ErrorPage() {
       {/* <div className="grid grid-cols-3" > */}
       <div className="md:w-20 "></div>
       <div className="relative w-[16rem] h-[4rem] flex justify-end self-center md:hidden">
-        <Image src={data.whitelogo} alt={"logo"} priority={true} fill />
+        <Image src={data.whitelogo} alt={"skiller logo"} priority={true} fill />
       </div>
       <div className="text-[40px] text-black flex justify-center self-center">
         {data.tagline}
       </div>
 
       <div className="hidden md:relative  md:h-[4rem] md:flex md:self-start md:justify-end">
-        <Image src={data.whitelogo} alt={"logo"} priority={true} fill />
+        <Image src={data.whitelogo} alt={"skiller logo"} priority={true} fill />
       </div>
       {/* </div> */}
       <div className="md:w-20 h-300 row-span-2"></div>
 
       <div className="flex w-full flex-col gap-[4rem] bg-grey items-center row-span-2">
         <div className="relative w-[18rem] h-[12rem] flex self-center justify-center">
-          <Image src={data.errorImage} alt={"logo"} priority={true} fill />
+          <Image src={data.errorImage} alt={"logo"} priority={true} fill/>
         </div>
         <div className="text-[40px] mt-[2rem] text-black">
           {data.errorMessage}
