cf52456a16826236b00b477066a459a840ed39e9
Kiberu-Nuhu <your_email@example.com>
2024-02-18 15:26:55 +0000

    buttons

--- components/searchBar/index.tsx
+++ components/searchBar/index.tsx
@@ -4,9 +4,15 @@ export default function SearchBar() {
   return (    
       <div>
         searchBar
-        <input className=" flex justify-center items-center rounded-full w-1/2 border-2 border-black placeholder-top-2" 
+        <div className="flex">
+        <input className=" pr-5 flex justify-center items-center rounded-full w-1/2 border-2 border-black placeholder-top-2" 
         placeholder ="Search for classes or tutors"
         type="text" />
+        <button className="flex justify-center items-center rounded-full border-2 border-black w-1/8 px-4 py-2 p-8">
+          Premium
+        </button>
+        </div>
+        
       </div>     
   );
 }
