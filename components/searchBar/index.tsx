ce366b636f540753085bc6f7cd9cc65d0165a698
khalifan-kfan <khalifanmuwonge@gmail.com>
2024-02-20 14:37:00 +0300

    search icon

--- components/searchBar/index.tsx
+++ components/searchBar/index.tsx
@@ -4,8 +4,8 @@ export default function SearchBar() {
   return (    
       <div>
         <div className=" flex items-center justify-center">
-       <input className=" py-1.5 pr-5 flex justify-center items-center rounded-full w-1/2 mr-7 border-2 border-black placeholder-top-2" 
-        placeholder ="&#xF002; Search for classes or tutors"
+       <input className=" py-1.5 pr-5 flex justify-center items-center rounded-full w-1/2 mr-7 border-2 border-black placeholder-top-2 placeholder-" 
+        placeholder ="&#x1F50E; Search for classes or tutors"
         type="text" />
         <button className=" mr-5 flex justify-center items-center rounded-full border-2 border-black w-1/8 px-4 py-1.5 p-8">
           Premium
