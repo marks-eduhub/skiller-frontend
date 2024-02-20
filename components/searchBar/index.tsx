ca94110b18f7f6bb06e143bcb2c0c8332f1451f6
Kiberu-Nuhu <your_email@example.com>
2024-02-20 11:51:39 +0000

    search icon

--- components/searchBar/index.tsx
+++ components/searchBar/index.tsx
@@ -4,18 +4,19 @@ export default function SearchBar() {
   return (    
       <div>
         <div className=" flex items-center justify-center">
-       <input className=" py-1.5 pr-5 flex justify-center items-center rounded-full w-1/2 mr-7 border-2 border-black placeholder-top-2" 
+        <input className=" py-1.5 pr-5 flex justify-center items-center rounded-full w-1/2 mr-7 border-2 border-black placeholder-top-2" 
         placeholder ="&#xF002; Search for classes or tutors"
         type="text" />
         <button className=" mr-5 flex justify-center items-center rounded-full border-2 border-black w-1/8 px-4 py-1.5 p-8">
           Premium
         </button>
+        <div className="">
         <button className=" bg-black text-white flex justify-center items-center rounded-full border-2 border-black w-1/8 px-4 py-1.5 p-8">
           Norah
         </button>
-       
         </div>
         
+        </div> 
       </div>     
   );
 }
