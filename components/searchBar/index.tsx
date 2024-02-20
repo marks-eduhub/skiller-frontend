fe1bddcb1747a427f86df6fcf23a464a9bd7df5c
Kiberu-Nuhu <your_email@example.com>
2024-02-20 11:08:06 +0000

    premium button

--- components/searchBar/index.tsx
+++ components/searchBar/index.tsx
@@ -3,7 +3,6 @@ import React from "react";
 export default function SearchBar() {
   return (    
       <div>
-        searchBar
         <div className=" flex items-center justify-center">
        <input className=" py-1.5 pr-5 flex justify-center items-center rounded-full w-1/2 mr-7 border-2 border-black placeholder-top-2" 
         placeholder ="&#xF002; Search for classes or tutors"
