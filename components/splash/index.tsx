6ec0f5e62c45a07e678fcfd020dc998ae57446af
mirembe-mariam <mirembemariam3@gmail.com>
2024-04-08 13:50:09 +0300

    user directed to dash

--- components/splash/index.tsx
+++ components/splash/index.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useEffect } from "react";
 
 import React from "react";
 import data from "./data.json";
@@ -6,17 +7,52 @@ import Image from "next/image";
 import { Button } from "../ui/button";
 import { TbArrowBadgeRight } from "react-icons/tb";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const Splash: React.FC = () => {
+  const router = useRouter();
+  const [button, setButton] = useState(false);
+
+  const handleDash = () => {
+    router.push("/dashboard");
+
+    sessionStorage.setItem("isLoggedIn", "true");
+    setButton(true);
+  };
+  useEffect(() => {
+    if (button) {
+      const isLoggedIn = sessionStorage.getItem("isLoggedIn");
+      if (isLoggedIn === "true") {
+        router.push("/dashboard");
+      }
+    }
+  }, []);
+
   return (
-    <div className="flex justify-center items-center h-screen bg-cover bg-no-repeat" style={{ backgroundImage: "url('/backgroundImage.svg')" }}>
+    <div
+      className="flex justify-center items-center h-screen bg-cover bg-no-repeat"
+      style={{ backgroundImage: "url('/backgroundImage.svg')" }}
+    >
       <div className="absolute top-1/4 transform -translate-y-1/2 text-center z-10">
         <p className="text-2xl font-bold text-black">{data.title}</p>
       </div>
       <div className="absolute bottom-16">
-        <button onClick={() => console.log("Let's Start button clicked")} className="px-4 py-2 bg-black text-white rounded-md">{data.buttonText}</button>
+        <button
+          onClick={handleDash}
+          className="px-4 py-2 bg-black text-white rounded-md"
+        >
+          {data.buttonText}
+        </button>
       </div>
-      <Image layout="intrinsic" width={375.99} height={618} src="/image.svg" alt="bulb" className=" z-0" style={{ maxWidth: "50%", maxHeight: "40%" }}  />
+      <Image
+        layout="intrinsic"
+        width={375.99}
+        height={618}
+        src="/image.svg"
+        alt="bulb"
+        className=" z-0"
+        style={{ maxWidth: "50%", maxHeight: "40%" }}
+      />
     </div>
   );
 };
