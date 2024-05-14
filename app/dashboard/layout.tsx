8b796fa5b7bd1a88a223a204a2be4bc91435d373
khalifan-kfan <khalifanmuwonge@gmail.com>
2024-05-14 21:17:25 +0300

    ch: clean up

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,8 @@
 import SideNav from '@/app/dashboard/sidenav';
 import { getSession } from '../../lib/userSS';
 import { redirect } from 'next/navigation';
+import Navbar from './NavBar';
+import Footer from "./Footer";
 
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
@@ -11,10 +13,15 @@ export default async function Layout({ children }: { children: React.ReactNode }
       }
   return (
     <div className="flex h-screen  flex-col md:flex-row md:overflow-hidden">
+
       <div className="w-full md:w-64">
         <SideNav/>
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+      <Navbar showGreeting />
+        {children}
+        <Footer/>
+        </div>
     </div>
   );
 }
