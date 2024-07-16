ab9f8593de2cbcac36b88bb6bb2fa219f6af87aa
Mirembe Mariam <mirembemariam3@gmail.com>
2024-07-16 20:20:25 +0300

    Course Overview Screen (#37)
    * course overview page
    
    * course overview screen
    
    * responsiveness
    
    * responsiveness

---
+++ components/Student/courseEnroll/courseOverview.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import Image from "next/image";
+
+const CourseOverview = () => {
+  const content = [
+    { title: "Getting started in UI/UX Design", duration: "10:12" },
+    {
+      title:
+        "Difference between user interface design and user experience design",
+      duration: "10:00",
+    },
+    { title: "Design Thinking process", duration: "15:30" },
+    { title: "The empathize phase", duration: "12:00" },
+    { title: "Define phase", duration: "17:00" },
+    { title: "The ideate phase", duration: "25:10" },
+    { title: "Prototype phase", duration: "30:00" },
+    { title: "Testing phase", duration: "17:39" },
+    { title: "Principles of design", duration: "22:25" },
+  ];
+  return (
+    <div className="">
+      <div className="flex flex-col gap-2">
+        <h1 className="text-xl mt-10 font-semibold">Brief Introduction</h1>
+        <p>
+          Condimentum cras tincidunt odio non. Sed dolor justo urna nibh sit
+          enim ac. Mattis cras mauris interdum lacus morbi. Velit leo non at
+          enim in. Eu duis a arcu cras sagittis adipiscing sit feugiat
+          facilisis. Arcu ridiculus et in quam vulputate. Nulla gravida est
+          tincidunt quam nec in fusce eros. Orci habitant sed leo amet sit. Urna
+          vitae sed mattis malesuada nunc et.
+        </p>
+      </div>
+      <div className="flex flex-col gap-2">
+        <h1 className="text-xl mt-10 font-semibold">What you&apos;ll learn</h1>
+
+        <div className="flex">
+          <Image src="/tick1.svg" width={20} height={20} alt="tick" />
+          <p className="ml-2">
+            Lorem ipsum dolor sit amet consectetur. Magna malesuada a diam
+            sapien urna turpis enim. Dignissim curabitur tincidunt facilisi
+            egestas sem metus lacinia a.
+          </p>
+        </div>
+        <div className="flex">
+          <Image src="/tick1.svg" width={20} height={20} alt="tick" />
+          <p className="ml-2">
+            Enim enim ipsum amet amet erat. Imperdiet blandit cursus et mauris
+            mi fames in. Facilisis in mauris fringilla netus
+          </p>
+        </div>
+        <div className="flex">
+          <Image src="/tick1.svg" width={20} height={20} alt="tick" />
+          <p className="ml-2">
+            Erat mauris commodo leo ac ac id. Faucibus fringilla lectus nec ac
+            phasellus.
+          </p>
+        </div>
+        <div className="flex">
+          <Image src="/tick1.svg" width={20} height={20} alt="tick" />
+          <p className="ml-2">
+            Lorem ipsum dolor sit amet consectetur. Magna malesuada a diam
+            sapien urna turpis enim. Dignissim curabitur tincidunt facilisi
+            egestas sem metus lacinia a.
+          </p>
+        </div>
+        <h1 className="text-xl mt-10 font-semibold">Requirements</h1>
+        <p>A well functioning computer, a notepad and an art book. </p>
+      </div>
+      <h1 className="text-xl mt-10 font-semibold mb-4">Course Content</h1>
+      <div className="w-full h-auto border border-black">
+        <div className="flex justify-between bg-gray-300 p-6 m-3">
+          <h1 className="font-semibold">Course Topics</h1>
+          <h1 className="font-semibold">Duration</h1>
+        </div>
+
+        <div className="divide-y divide-gray-300">
+          {content.map((item, index) => (
+            <div
+              key={index}
+              className="flex items-center w-full justify-between p-6"
+            >
+              <h1 className="font-semibold">{item.title}</h1>
+              <h1 className="font-semibold">{item.duration}</h1>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default CourseOverview;
---
+++ components/Student/courseEnroll/courseReviews.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import Image from "next/image";
+
+interface CourseReview {
+  name: string;
+  image: string;
+  comment: string;
+  rating: number;
+}
+
+interface CourseReviewProps {
+  reviews: CourseReview[];
+}
+
+const CourseReview: React.FC<CourseReviewProps> = ({ reviews }) => {
+  if (reviews.length === 0) {
+    return <div>No reviews available.</div>;
+  }
+  return (
+    <div className="w-full">
+      <h1 className="sm:mt-3 mt-5 font-bold text-xl">
+        No. of reviews: {reviews.length}
+      </h1>
+      <Image src="/reviews1.svg" alt="reviews" width={120} height={120} />
+      <div className="mt-10 space-y-6">
+        {reviews.map((review) => (
+          <div
+            key={review.name}
+            className="w-full h-auto border border-gray-300 rounded-lg p-4"
+          >
+            <div className="flex items-center mb-2">
+              <Image
+                src={review.image}
+                alt={review.name}
+                width={40}
+                height={40}
+                className="rounded-full"
+              />
+              <div className="ml-3">
+                <h2 className="font-semibold">{review.name}</h2>
+                <div className="flex items-center">
+                  {Array.from({ length: 5 }, (_, index) => (
+                    <svg
+                      key={index}
+                      aria-hidden="true"
+                      className={`w-5 h-5 ${
+                        index < Math.floor(review.rating)
+                          ? "text-yellow-300"
+                          : "text-gray-300"
+                      }`}
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <title>{`Star ${index + 1}`}</title>
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.184 3.63a1 1 0 00.95.69h3.813c.969 0 1.371 1.24.588 1.81l-3.088 2.25a1 1 0 00-.364 1.118l1.184 3.63c.3.921-.755 1.688-1.538 1.118l-3.088-2.25a1 1 0 00-1.175 0l-3.088 2.25c-.783.57-1.838-.197-1.538-1.118l1.184-3.63a1 1 0 00-.364-1.118l-3.088-2.25c-.783-.57-.381-1.81.588-1.81h3.813a1 1 0 00.95-.69l1.184-3.63z" />
+                    </svg>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <p className="text-gray-700">{review.comment}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CourseReview;
