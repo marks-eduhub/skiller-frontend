484e8bf180e69e11b786edf8a5e3a7971152b5c7
mirembe-mariam <mirembemariam3@gmail.com>
2024-08-20 15:08:23 +0300

    auth

--- components/Student/Login/loginForm.tsx
+++ components/Student/Login/loginForm.tsx
@@ -8,6 +8,7 @@ import data from "./data.json";
 import { dotPulse } from "ldrs";
 import { useAuthContext } from "@/Context/AuthContext";
 import { login } from "../../../lib/login";
+import { message } from "antd";
 
 dotPulse.register();
 
@@ -19,6 +20,8 @@ export default function LogIn() {
   const { mutate, isPending, isError, error } = useMutation({
     mutationFn: login,
     onSuccess: (data) => {
+      message.success(`Welcome back ${data.user.username}`);
+
       if (setUser) {
         setUser(data.user);
       }
