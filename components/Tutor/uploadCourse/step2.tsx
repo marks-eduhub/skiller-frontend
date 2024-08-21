51fa44fdbc5e11fe3c31c0fe073ac57017f687e1
mirembe-mariam <mirembemariam3@gmail.com>
2024-08-21 11:07:54 +0300

    topic fixes

--- components/Tutor/uploadCourse/step2.tsx
+++ components/Tutor/uploadCourse/step2.tsx
@@ -1,14 +1,10 @@
-"use client";
 import React, { useState } from "react";
 import Image from "next/image";
-
-import Step from "./step";
-
+import TopicForm from "./topicForm";
 const Step2 = () => {
   const [topics, setTopics] = useState<string[]>([]);
   const [showForm, setShowForm] = useState(false);
   const [isAdding, setIsAdding] = useState(false);
-  
 
   const addTopic = () => {
     setIsAdding(true);
@@ -18,33 +14,25 @@ const Step2 = () => {
   const deleteTopic = (indexToDelete: number) => {
     setTopics(topics.filter((topic, index) => index !== indexToDelete));
   };
-
   return (
     <div className="p-4">
-     {topics.length === 0 && !isAdding && (
-        <p className="font-bold items-center justify-center flex text-[20px] mt-10">
-          No topics! Create one.
-        </p>
-      )}
-
       {topics.map((topic, index) => (
-        <Step
+        <TopicForm
           key={index}
           name={topic}
           deleteTopic={() => deleteTopic(index)}
         />
       ))}
-
-      <div className="flex mt-7 mb-5 justify-end">
-        <div
-          onClick={addTopic}
-          className="flex gap-2 bg-black w-[180px] h-[40px] px-9 py-7 items-center justify-center rounded-lg cursor-pointer"
-        >
-          <Image src="/plus.svg" alt="add topic" width={20} height={20} />
-          <p className="text-white">Add topic</p>
+        <div className="border border-gray w-full mt-6 p-3">
+          <div
+            className="w-full bg-gray-200 h-[90px] cursor-pointer flex items-center justify-center gap-2"
+            onClick={addTopic}
+          >
+            <Image src="/pluss.svg" alt="pluss" width={20} height={20} />
+            <p>Add your topics</p>
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
--- components/Tutor/uploadCourse/topicForm.tsx
+++ components/Tutor/uploadCourse/topicForm.tsx
@@ -12,8 +12,7 @@ interface TopicProps {
   name: string;
   deleteTopic: () => void;
 }
-
-const Step: React.FC<TopicProps> = ({ name, deleteTopic }) => {
+const TopicForm: React.FC<TopicProps> = ({ name, deleteTopic }) => {
   const [expanded, setExpanded] = useState(false);
   const [uploadImage, setUploadImage] = useState<string | null>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -21,7 +20,6 @@ const Step: React.FC<TopicProps> = ({ name, deleteTopic }) => {
   const toggleExpanded = () => {
     setExpanded(!expanded);
   };
-
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (file) {
@@ -32,7 +30,6 @@ const Step: React.FC<TopicProps> = ({ name, deleteTopic }) => {
       reader.readAsDataURL(file);
     }
   };
-
   const openModal = () => {
     setIsModalOpen(true);
   };
@@ -45,11 +42,27 @@ const Step: React.FC<TopicProps> = ({ name, deleteTopic }) => {
     deleteTopic();
     closeModal();
   };
-
   return (
-    <div className="border border-gray-200 p-6 flex flex-col gap-3">
+    <div className="flex flex-col mt-5 mb-5 cursor-pointer">
+      <div className="w-full h-[100px] bg-gray-300 mt-3 ">
+        <div
+          onClick={toggleExpanded}
+          className="flex items-center justify-between p-9 relative"
+        >
+          <h1 className="font-bold text-[20px]">{name}</h1>
+          <div
+            className={`transition-transform duration-200 transform ${
+              expanded ? "rotate-90" : ""
+            }`}
+          >
+            {/* <Image src="/closedarrow.svg" alt="arrow" width={10} height={10} /> */}
+            <Image src="/edit.svg" alt="arrow" width={20} height={20} />
+
+          </div>
+        </div>
+      </div>
       {expanded && (
-        <div>
+        <div className="p-4 w-full h-auto bg-gray-300 rounded-md overflow-hidden break-words">
           <div className="mt-5 flex items-center w-full">
             <label className="flex-shrink-0">Topic name</label>
             <input
@@ -57,27 +70,37 @@ const Step: React.FC<TopicProps> = ({ name, deleteTopic }) => {
               className="border ml-5 border-black w-full bg-[#F9F9F9] px-3 py-2 outline-none"
             />
           </div>
-          <div className="mb-10 mt-4">
+          <div className="mb-10 mt-4 ">
             <label className="block text-sm font-medium mb-4 mt-6">
               Enter a brief description about the topic
             </label>
-            <ReactQuill
-              placeholder="Write content here"
-              className="bg-white h-40"
-            />
+            <div className="bg-white w-full overflow-hidden">
+              <ReactQuill
+                placeholder="Write content here"
+                className="bg-white h-[200px]"
+              />
+            </div>
           </div>
           <div>
             <div className="mb-6 mt-10">
               <label className="block text-sm font-medium mb-4 mt-20">
                 What will the student learn?
               </label>
-              <ReactQuill className="h-40" />
+              <div className="bg-white w-full overflow-hidden">
+                <ReactQuill
+                  placeholder="Write content here"
+                  className="bg-white h-[200px]"
+                />
+              </div>
             </div>
           </div>
           <div>
             <div className="mt-20">
               <h1>Upload Video</h1>
-              <div className="flex flex-col mt-5 items-center justify-center border border-dashed border-black p-3 relative h-[200px] rounded"  onChange={handleImageChange}>
+              <div
+                className="flex flex-col mt-5 items-center justify-center border border-dashed border-black p-3 relative h-[200px] rounded"
+                onChange={handleImageChange}
+              >
                 <GrCloudUpload className="text-blue-800 w-10 h-10" />
                 <span className="text-gray-500">
                   Drag & drop files or
@@ -106,15 +129,6 @@ const Step: React.FC<TopicProps> = ({ name, deleteTopic }) => {
           </div>
         </div>
       )}
-      <div className="w-full h-[100px] bg-gray-300  cursor-pointer">
-        <div
-          className="flex items-center  justify-center gap-1 p-9"
-          onClick={toggleExpanded}
-        >
-          <Image src="/pluss.svg" alt="plus" width={20} height={20} />
-          <h1 className="font-semibold text-[18px]">Add your topics</h1>
-        </div>
-      </div>
       <CustomModal
         isOpen={isModalOpen}
         onClose={closeModal}
@@ -124,4 +138,4 @@ const Step: React.FC<TopicProps> = ({ name, deleteTopic }) => {
   );
 };
 
-export default Step;
+export default TopicForm;
