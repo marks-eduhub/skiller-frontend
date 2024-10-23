4613587169fda815e13be9a3a438de2abcafa086
Mirembe Mariam <mirembemariam3@gmail.com>
2024-10-23 12:58:02 +0300

    Fetching carousel data from strapi (#54)
    * fetching featured courses from strapi
    
    * corrections
    
    * added true to some courses

--- components/Student/courseCards/cardDescriptionBar.tsx
+++ components/Student/courseCards/cardDescriptionBar.tsx
@@ -1,34 +1,33 @@
 import { PersonIcon, ClockIcon, StarFilledIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
-interface ProductDetails {
-  instructor: string;
-  duration: string;
-  rating: number;
-  description: string;
-  topic: string;
-  level: string;
+
+interface ProductDetailsProps {
   days: string;
+  description: string;
+  duration: string;
+  level: string;
+  rating: number;
+  tutorName: string;
 }
 
-const ProductDescriptionBar: React.FC<ProductDetails> = ({
-  instructor,
-  duration,
-  rating,
-  description,
-  topic,
-  level,
+const ProductDescriptionBar: React.FC<ProductDetailsProps> = ({
   days,
+  description,
+  duration,
+  level,
+  rating,
+  tutorName,
 }) => {
   return (
     <>
-      <div className="relative bg-black px-4 sm:py-2 py-3 text-white max-md:hidden ">
+      <div className="relative bg-black px-4 pt-2 pb-8 text-white max-md:hidden ">
         <div className="relative ">
           <h3 className="absolute bottom-0 right-0  font-semibold text-white">
             {days}
           </h3>
 
           <h3 className="absolute top-0 right-0  font-semibold text-white">
-            {topic}
+            {description}
           </h3>
 
           <div className="flex flex-col md:items-center mb-2 mt-2 ">
@@ -39,15 +38,11 @@ const ProductDescriptionBar: React.FC<ProductDetails> = ({
             <p className="text-sm text-white mt-2">{level}</p>
           </div>
 
-          <div className="flex items-center mb-4 absolute top-0 left-0 ">
-            <Image
-              src="/person.svg"
-              alt="person"
-              width={20}
-              height={20}
-              className="w-6 h-6 mr-2"
-            />
-            <h3 className="text-white font-semibold">{instructor}</h3>
+          <div className="flex items-center mb-4 absolute gap-2 top-0 left-0 ">
+            <div className="w-6 h-6 relative">
+              <Image src="/person.svg" alt="person" fill />
+            </div>
+            <h3 className="text-white font-semibold">{tutorName}</h3>
           </div>
 
           <div className="absolute bottom-0 left-0 flex items-center  ">
@@ -57,30 +52,32 @@ const ProductDescriptionBar: React.FC<ProductDetails> = ({
         </div>
       </div>
 
-      <div className=" text-white sm:hidden relative bg-black px-4  py-3  ">
+      <div className=" text-white sm:hidden relative bg-black px-4  py-3 h-full  ">
         <div className="relative flex flex-col ">
-          <h3 className=" font-semibold text-white">{topic}</h3>
-          <h3 className=" font-semibold text-white">{days}</h3>
-          <div className="flex items-center mt-4 mb-2 ">
+          <h3 className=" font-semibold text-white">{description}</h3>
+          <h3 className=" font-semibold text-white mt-2 ">{days}</h3>
+          <div className="flex items-center mt-4 mb-2 gap-2 ">
+            <div className="w-6 h-6 relative">
             <Image
               src="/person.svg"
               alt="person"
-              width={20}
-              height={20}
-              className="w-6 h-6 mr-2"
+             fill
             />
-            <h3 className="text-white font-semibold">{instructor}</h3>
+            </div>
+            <h3 className="text-white font-semibold">
+              {tutorName}
+            </h3>
           </div>
           <div className="flex  justify-between w-full items-center mb-2 mt-2 ">
             <div className="flex items-center">
               <ClockIcon className="w-6 h-6 mr-2" />
               <p className="text-white">{duration}</p>
             </div>
-            <p className="text-sm text-white mt-2">{level}</p>
+            <p className="text-sm text-white mt-2 ">{level}</p>
           </div>
 
-          <div className=" flex items-center  ">
-            <StarFilledIcon className="w-6 h-6 mr-2" />
+          <div className=" flex items-center pt-5 ">
+            <StarFilledIcon className="w-6 h-6 " />
             <h3 className="text-white font-semibold">{rating}</h3>
           </div>
         </div>
--- lib/types.tsx
+++ lib/types.tsx
@@ -56,6 +56,56 @@ export interface RegisterResponse {
   user: {
     id: string;
     username: string;
+
+  }
+  export interface CarouselCourses {
+    id: number;
+   tutors: string;
+    Image: string;
+    rating: number;
+    duration: string;
+    description: string
+    level: string;
+    days: string;
+  }
+  export interface Category {
+    id: number;
+    coursecategories: string;
+    slug: string;
+    categorySlug: string;
+    courses: Course[];
+  }
+  export interface Course {
+    id: number;
+    attributes: {
+      course: any;
+      coursename: string;
+      rating: number;
+      duration: string;
+      tutor: string;
+      card: string;
+      categories: { data: Category[] };
+    };
+  }
+  export interface RegisterResponse {
+    jwt: string;
+    user: {
+      id: string;
+      username: string;
+      email: string;
+    };
+  }
+  export interface AuthContextType {
+    user: User | undefined;
+    setUser: (user: User) => void;
+    isLoading: boolean;
+  }
+  
+  export const AuthContext = createContext<AuthContextType>({
+    user: undefined,
+    setUser: () => {}, 
+    isLoading: false,
+  });
     email: string;
   };
 }
