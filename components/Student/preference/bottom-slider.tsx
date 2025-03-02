4ea377510d5f500e8c45b0bf6f34843d16a1191c
emmilly immaculate <57020210+maqamylee0@users.noreply.github.com>
2025-03-02 15:14:37 +0300

    Bio update (#70)
    * preference layout
    
    * implement preferences and bio
    
    * fix tanstackquery version
    
    ---------
    
    Co-authored-by: khalifan-kfan <khalifanmuwonge@gmail.com>

--- components/Student/preference/bottom-slider.tsx
+++ components/Student/preference/bottom-slider.tsx
@@ -2,47 +2,82 @@ import Image from "next/image";
 import { TbArrowBadgeRight } from "react-icons/tb";
 import data from "./data.json";
 import withAuth from "@/components/AuthProvider/Auth";
-
+import { useAuthContext } from "@/Context/AuthContext";
+import { useFetchUserDetails } from "@/hooks/useProfile";
+import { message } from "antd";
+import { useRouter } from "next/navigation";
 interface SliderProps {
   data2: {
     currentIndex: number;
   };
   nextSlide: () => void;
+  prevSlide: () => void; // Added function to go back
   skipToSplash: () => void;
+  handleSubmitBio: () => void;
+  handleSubmitPreferences: () => void;
+  formData: {
+    phone: string;
+    gender: string;
+    date_birth: string;
+  };
+  prefData: Record<string, any>;
 }
 
 const BottomSlider: React.FC<SliderProps> = ({
   data2,
   nextSlide,
+  prevSlide, // Accept prevSlide function
   skipToSplash,
+  handleSubmitBio,
+  handleSubmitPreferences,
+  formData,
+  prefData,
 }) => {
+  const router = useRouter();
+
   const lastIndex = 2;
 
-  const handleLastSlide = () => {
+  const handleLastSlide = async () => {
     if (data2.currentIndex === lastIndex - 1) {
-      skipToSplash();
+      if (Object.keys(prefData).length > 0) {
+        handleSubmitPreferences();
+      } else {
+        router.push("/splash");
+      }
     } else {
-      nextSlide();
+      if (formData.phone && formData.gender && formData.date_birth) {
+        await handleSubmitBio();
+        nextSlide();
+      } else {
+        nextSlide();
+      }
     }
   };
-
   return (
     <>
+      {/* Desktop View */}
       <div className="hidden md:flex md:justify-end md:self-center md:col-span-1">
         <button
           className="rounded-[7px] border-4 border-solid border-black text-black text-[20px] w-[145px] h-[60px]"
-          onClick={skipToSplash}
+          onClick={data2.currentIndex === 1 ? prevSlide : skipToSplash} // Show "Back" on second slide
         >
-          <p className="break-all">{data.skip}</p>
+          <p className="break-all">
+            {data2.currentIndex === 1 || data2.currentIndex === 2
+              ? "Back"
+              : data.skip}
+          </p>
         </button>
       </div>
+
       <div className="hidden md:relative md:col-span-2 md:h-[20px] md:flex md:justify-center md:self-center cursor-pointer">
         {data2.currentIndex === 0 && (
           <Image
             src={data.slider1}
             alt={"slider icon"}
             priority={true}
-            fill
+            // fill
+            width={150}
+            height={200}
             onClick={handleLastSlide}
           />
         )}
@@ -51,7 +86,9 @@ const BottomSlider: React.FC<SliderProps> = ({
             src={data.slider}
             alt={"slider icon"}
             priority={true}
-            fill
+            // fill
+            width={150}
+            height={200}
             onClick={handleLastSlide}
           />
         )}
@@ -60,11 +97,14 @@ const BottomSlider: React.FC<SliderProps> = ({
             src={data.slider2}
             alt={"slider icon"}
             priority={true}
-            fill
+            // fill
+            width={150}
+            height={200}
             onClick={handleLastSlide}
           />
         )}
       </div>
+
       <div className="hidden md:flex md:justify-start md:self-center md:col-span-1">
         <button
           onClick={handleLastSlide}
@@ -75,10 +115,9 @@ const BottomSlider: React.FC<SliderProps> = ({
         </button>
       </div>
 
-      {/* on smaall screens */}
-
+      {/* Mobile View */}
       <div className="sm:hidden">
-        <div className="relative  my-10 flex items-center justify-center self-center cursor-pointer">
+        <div className="relative my-10 flex items-center justify-center self-center cursor-pointer">
           {data2.currentIndex === 0 && (
             <Image
               src={data.slider1}
@@ -110,23 +149,24 @@ const BottomSlider: React.FC<SliderProps> = ({
             />
           )}
         </div>
-        <div className="flex justify-between mt-5 items-center w-full ">
+
+        <div className="flex justify-between mt-5 items-center w-full">
           <button
             className="rounded-[7px] border-4 border-solid border-black text-black text-[20px] w-[145px] h-[60px]"
-            onClick={skipToSplash}
+            onClick={data2.currentIndex === 1 ? prevSlide : skipToSplash} // Show "Back" on second slide
           >
-            <p className="break-all">{data.skip}</p>
+            <p className="break-all">
+              {data2.currentIndex === 1 ? "Back" : data.skip}
+            </p>
           </button>
 
-          <div className="sm:hidden md:flex md:justify-start md:self-center md:col-span-1">
-            <button
-              onClick={handleLastSlide}
-              className="flex justify-center items-center bg-black rounded-[7px] w-[145px] h-[60px]"
-            >
-              <TbArrowBadgeRight size={30} color="white" />
-              <p className="text-white">Next</p>
-            </button>
-          </div>
+          <button
+            onClick={handleLastSlide}
+            className="flex justify-center items-center bg-black rounded-[7px] w-[145px] h-[60px]"
+          >
+            <TbArrowBadgeRight size={30} color="white" />
+            <p className="text-white">Next</p>
+          </button>
         </div>
       </div>
     </>
