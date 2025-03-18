26cb58f6939fafc58f2a8cf91c1ec0a437921901
mirembe-mariam <mirembemariam3@gmail.com>
2025-03-18 19:16:10 +0300

    recent courses

--- hooks/useComments.tsx
+++ hooks/useComments.tsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import api from "@/lib/axios";
 import {useQuery} from "@tanstack/react-query";
 
--- hooks/useRecentCourses.tsx
+++ hooks/useRecentCourses.tsx
@@ -4,7 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 
 const fetchRecentCourses = async (userId: number) => {
   const response = await api.get(
-    `/api/recent-courses?filters[user][id][$eq]=${userId}&sort=dateAccessed:desc&populate[course][populate]=card,tutor`
+    `/api/recent-courses?filters[user][id][$eq]=${userId}&sort=dateLastAccessed:desc&populate[course][populate]=card,tutor`
   );
 
   return response.data;
@@ -30,14 +30,35 @@ export const useRecentCourses = () => {
 
 export const addRecentCourse = async (courseId: number, userId: number) => {
   try {
-    const response = await api.post("/api/recent-courses", {
-      data: {
-        course: courseId,
-        user: userId,
-        dateAccessed: new Date().toISOString(),
-      },
-    });
-    return response.data;
+    const existingEntry = await api.get(
+      `/api/recent-courses?filters[user][id][$eq]=${userId}&filters[course][id][$eq]=${courseId}&sort=dateLastAccessed:desc&populate[course][populate]=card,tutor`
+    );
+
+    if (existingEntry.data.data.length > 0) {
+      const recentCourse = existingEntry.data.data[0];
+      const updatedCount = (recentCourse.attributes.timesAccessed || 0) + 1;
+
+      const response = await api.put(`/api/recent-courses/${recentCourse.id}`, {
+        data: {
+          timesAccessed: updatedCount,
+          dateLastAccessed: new Date().toISOString(),
+        },
+      });
+
+      return response.data;
+    } else {
+      const response = await api.post("/api/recent-courses", {
+        data: {
+          course: courseId,
+          user: userId,
+          dateFirstAccessed: new Date().toISOString(),
+          dateLastAccessed: new Date().toISOString(),
+          timesAccessed: 1,
+        },
+      });
+
+      return response.data;
+    }
   } catch (error) {
     throw error;
   }
